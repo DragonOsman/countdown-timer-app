15db19338cf46efd6a141a5fe0210a9695c3afa1
Osman Zakir <osmanzakir90@hotmail.com>
2020-01-03 19:53:58 +0500

    inital commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## DragonOsman's Countdown Timer
+
+This is my countdown timer app.  The user has to choose a date and time to count down to, with the date and time field being required, and there's also a (required) field to enter the event name.  There is also a button to stop the timer in addition to the for starting it.  
---
+++ scripts/script.js
@@ -0,0 +1,69 @@
+"use strict";
+
+function getInput() {
+  const input = document.getElementById("input").value;
+  const deadline = new Date(input);
+  return deadline;
+}
+
+function leapYear(year) {
+  if (year % 4 !== 0) {
+    if (year % 400 !== 0 && year % 100 !== 4) {
+      return false;
+    }
+    return false;
+  }
+  return true;
+}
+
+function initializeClock() {
+  const deadline = getInput();
+  if (deadline < Date.now()) {
+    const errorPara = document.createElement("p");
+    errorPara.textContent = "Sorry; we don't count up from a past event";
+    document.querySelector("form").prepend(errorPara);
+    errorPara.insertAdjacentHTML("afterend", "<br />");
+  } else {
+    const timerId = setInterval(() => {
+      const now = new Date().getTime();
+      const timeDiff = deadline - now;
+      let february = 0;
+      if (leapYear(deadline.getFullYear())) {
+        february = 29;
+      } else {
+        february = 28;
+      }
+
+      const daysInMonth = [31, february, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+      let days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+      const months = Math.floor(days / daysInMonth[deadline.getMonth()]);
+      days %= daysInMonth[deadline.getMonth()];
+      const hours = Math.floor((timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+      document.getElementById("day").textContent = days;
+      document.getElementById("month").textContent = months;
+      document.getElementById("hour").textContent = hours;
+      document.getElementById("minute").textContent = minutes;
+      document.getElementById("second").textContent = seconds;
+      if (timeDiff < 0) {
+        clearInterval(timerId);
+        const event = document.getElementById("event");
+        document.getElementById("is-it-time").textContent = `${event} is here!`;
+        document.getElementById("month").textContent = "0";
+        document.getElementById("day").textContent = "0";
+        document.getElementById("hour").textContent = "0";
+        document.getElementById("minute").textContent = "0";
+        document.getElementById("second").textContent = "0";
+      }
+
+      const stopBtn = document.getElementById("stop-btn");
+      stopBtn.addEventListener("click", function() {
+        clearInterval(timerId)
+      });
+    }, 1000);
+  }
+}
+
+const startBtn = document.getElementById("start-btn");
+startBtn.addEventListener("click", initializeClock);
